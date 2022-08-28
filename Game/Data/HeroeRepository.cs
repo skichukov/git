f99b02909f25722504c374badc169c665f9e1c55
Стилиян <stu1901321020@uni-plovdiv.bg>
2022-08-28 22:39:27 +0300

    Changes_8_28

--- Game/Data/HeroeRepository.cs
+++ Game/Data/HeroeRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Game.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,26 @@ namespace Game.Data
 {
     internal class HeroeRepository
     {
+        SqlConnection s = new SqlConnection(Properties.Settings.Default.data);
+
+        public void CreateHero(Character ch)
+        {
+            s.Open();
+            string query = "Insert into Characters ([UserId], [CharacterName], [TYPE]," +
+                " [AttackPoints], [DefencePoints], [HealthPoints], [DateCreated])" +
+                " values(@UserId, @CharacterName, @TYPE, @AttackPoints, @DefencePoints," +
+                " @HealthPoints, @DateCreated)";
+            SqlCommand command = new SqlCommand(query, s);
+            command.Parameters.Add("@UserId", SqlDbType.Int).Value = ch.UserId;
+            command.Parameters.Add("@CharacterName", SqlDbType.NVarChar).Value = ch.CharacterName;
+            command.Parameters.Add("@TYPE", SqlDbType.NVarChar).Value = ch.Type;
+            command.Parameters.Add("@AttackPoints", SqlDbType.Int).Value = ch.AttackPoints;
+            command.Parameters.Add("@DefencePoints", SqlDbType.Int).Value = ch.DefencePoints;
+            command.Parameters.Add("@HealthPoints", SqlDbType.Int).Value = ch.HealthPoints;
+            command.Parameters.Add("@DateCreated", SqlDbType.DateTime).Value = ch.DateCreated;
+
+            object result = command.ExecuteNonQuery();
+            s.Close();
+        }
     }
 }
