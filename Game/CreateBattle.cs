3395e07416922a58058457524f33f15afcda786b
Стилиян <stu1901321020@uni-plovdiv.bg>
2022-09-11 22:48:51 +0300

    Changes_9_11

--- Game/CreateBattle.cs
+++ Game/CreateBattle.cs
@@ -53,18 +53,60 @@ namespace Game
             mf.Show();
         }
 
+        public int abs(int a)
+        {
+            int result;
+            if(a < 0)
+            {
+                result = -a;
+            }
+            else
+            {
+                result = a;
+            }
+
+            return result;
+        }
+
         private void btnCreate_Click(object sender, EventArgs e)
         {
             Character hero_1 = hs.GetHeroById((int)iDComboBox.SelectedValue);
             Character hero_2 = hs.GetHeroById((int)iDComboBox1.SelectedValue);
             Random r = new Random();
+            int health_upd;
+
+            Battle b = new Battle
+            {
+                UserId = loggedUser.Id,
+                DateCreated = DateTime.Now,
+                Organiser = loggedUser
+            };
 
             while((hero_1.HealthPoints > 0) && (hero_2.HealthPoints > 0))
             {
                 int rand = r.Next(-5, 5);
                 int new_attack = hero_1.AttackPoints + rand;
                 hero_1.AttackPoints = new_attack;
-            } 
+                int difference = abs(hero_1.AttackPoints - hero_2.DefencePoints);
+                if(hero_1.AttackPoints > hero_2.DefencePoints)
+                {
+                    health_upd = hero_2.HealthPoints - difference;
+                    hero_2.HealthPoints = health_upd;
+                    MessageBox.Show("Атаката на " + hero_1.CharacterName + " става " 
+                        + hero_1.AttackPoints +  " Защитата на " + hero_2.CharacterName + 
+                        " e " + hero_2.DefencePoints);
+                }
+                else
+                {
+                    health_upd = hero_1.HealthPoints - difference;
+                    hero_1.HealthPoints = health_upd;
+                    MessageBox.Show("Атаката на " + hero_1.CharacterName + " става "
+                        + hero_1.AttackPoints + " Защитата на " + hero_2.CharacterName +
+                        " e " + hero_2.DefencePoints);
+                }
+            }
+
+            _ = (hero_1.HealthPoints > 0) ? (b.Winner = hero_1, b.Loser = hero_2) : (b.Winner = hero_2, b.Loser = hero_1);
         }
     }
 }
--- Game/Models/Round.cs
+++ Game/Models/Round.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,16 @@ namespace Game.Models
 {
     internal class Round
     {
+        [Key]
+        public int Id { get; set; }
+        public int BattleId { get; set; }
+        public int Character1State { get; set; }
+        public int Character2State { get; set; }
+        public int RoundIndex { get; set; }
+        public int RandomNumber { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        [ForeignKey("BattleId")]
+        public Battle ParrentBattle { get; set; }
     }
 }
